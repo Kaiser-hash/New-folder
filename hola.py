aed96b6b8df30f6a5c8e9cc8bd0cf73f8ca3f8e0
Kaiser-hash <bachi9383@gmail.com>
2025-04-15 13:28:12 -0400

    clase1 python

---
+++ hola.py
@@ -0,0 +1,75 @@
+
+
+#  declararcion de variables
+# nombre=input()
+# # ejemplo de contatenacion
+# edad=input()
+# print("hola", nombre, "y tu edad es", edad)
+
+# print("ingrese 2 numeros")
+
+# n1=int(input("ingrese un numero"))
+# n2=int(input("ingrese un numero"))
+# print("el resultado de la suma es", n1+n2)
+
+# promedip
+
+# print("ingrese 3 numeros para sacar su promedio")
+# n1=int(input("ingrese un numero"))
+# n2=int(input("ingrese un numero"))
+# n3=int(input("ingrese un numero"))
+# prom=(n1+n2+n3)/3
+# print("el promedio es", prom)
+
+# if prom>=40:
+#     print("el alumno aprobo")
+# else:
+#     print("el alumno reprobo")
+
+
+# verificacion de mayoria de edad
+
+# edad=int(input("ingrese su edad"))
+
+# if edad>=18:
+#     print("usted es mayor de edad")
+# else:
+#     print("usted es menor de edad")
+
+# niño menor de 12
+# adolecente entre 12 y 17
+# mayor de edad mas o igual a 18
+
+# edad = int(input("ingrese su edad: "))
+
+# if edad < 12:
+#     print("tiene", edad, "es un niño")
+# elif edad >= 12 and edad <= 17:
+#     print("tiene", edad, "es adolecente")
+# else:
+#     print("tiene", edad, "es mayor de edad")
+
+
+
+
+n1=int(input("ingrese un numero"))
+n2=int(input("ingrese otro numero"))
+n3=int(input("ingrese otro numero"))
+
+if n1>n2 and n1<n3:
+    print("el numero", n1, "es el mayor")
+elif n2>n3:
+        print("el numero", n2, "es el mayor")
+else:
+    print("el numero", n3, "es el mayor")
+
+
+
+
+
+
+
+
+
+
+
