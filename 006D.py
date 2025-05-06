1beb0d4855a55f23c23430ec9e158c709f65980d
Kaiser <bachi9383gmail.com>
2025-05-06 13:14:52 -0400

    practica

---
+++ 006D.py
@@ -0,0 +1,52 @@
+# print("bienvenido al sistema de credito")
+# ingreso = int(input("ingrese un ingreso mensual: "))
+# nivel_educacional = (input("ingrese su nivel educacional (basico, medio, superior): ")).lower()
+# nacionalidad=(input("ingrese su nacionalidad (chileno, extranjero): ")).lower()
+
+# """desayuno"""
+# import time
+
+# pan=250
+
+# while pan!=0:
+#     print("ñam, le queda",pan, "grs de pan")
+#     pan-=50
+#     time.sleep(2)
+
+# chocolate=True
+
+# if chocolate:
+#     print("puede comprar")
+# else:
+#     print("no puede comprar")
+
+# print("ingrese 2 numeros")
+# num1 = int(input("ingrese el primer numero"))
+# num2 = int(input("ingrese el sehundo numero"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
