1c1317a45442d08d92d05acfe11b26395ec5a96b
Kaiser <bachi9383@gmail.com>
2025-05-15 13:21:24 -0400

    ejercicios

---
+++ veriferror.py
@@ -0,0 +1,72 @@
+
+#´´´
+#  crear un menu de carrito con las siguientes opciones
+# 1.- ingresar nombre del usuario 
+# sera mostrado en la boleta, con un saludo
+# 2.- comprar, poner productos para poder comprar
+# con un precio correspondiente
+# 3.- sacar boleta, calcular el precio neto 
+# y el precio con IVA. mostrar totales
+
+
+# 4.- salir
+# con 
+# por lo menos 3 productos
+# no hay limite de compra
+# se puede salir en cualquier momento
+# los montos de los productos son fijos
+# ´´´
+
+'''ingrese cant de alumnos: 5
+ingrese cantidad del alumno 1 :3
+ingrese la nota 1 del alumno 1
+ingrese la nota 2 del alumno 1
+ingrese la nota 3 del alumno 1
+El promedio del alumno 1 es : 5,6
+el alumno 1 aprobo
+ingrese la nota 1 del alumno 2
+'''
+alumno=int(input("ingrese la cantidad de alumnos "))
+
+cant_notas=0
+
+notas=0
+suma=0
+
+promedio=0
+promedio_total=0
+
+for i in range(alumno):
+    cant_notas=int(input("ingrese la cantidad de notas"))
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
